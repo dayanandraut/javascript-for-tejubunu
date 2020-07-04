0b1af42de533aae99adc35e06a893a68ddf515ae
dayanandraut <dayanandraut2015@gmail.com>
2020-07-04 17:13:59 +0530

    average of numbers in array

---
+++ average.js
@@ -0,0 +1,12 @@
+let marks = [20,70,90,27,45]
+
+let sum = 0
+
+for(let i=0; i<marks.length;i++){
+  sum = sum + marks[i]
+}
+
+console.log(sum)
+
+let average = sum / marks.length
+console.log(average)
